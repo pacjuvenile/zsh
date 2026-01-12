d2e960f8f5804f72627ceb0f1c5d37ad56fdf811
packjuvenile <3283890195@qq.com>
2026-01-12 14:10:59 +0800

    modified:   plugins/powerlevel10k.zsh

--- plugins/powerlevel10k.zsh
+++ plugins/powerlevel10k.zsh
@@ -1,6 +1,4 @@
-# 启用 Powerlevel10k 的即时提示功能
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-# 运行`p10k configure`或直接编辑 ~/.p10k.zsh以自定义提示符
+# 主题美化
+zinit ice depth"1"  # git clone depth
+zinit light romkatv/powerlevel10k
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
