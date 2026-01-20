bd9f0aee9c1e9e7a27eac7b5120ad4c0cc9f985b
packjuvenile <3283890195@qq.com>
2026-01-20 10:24:23 +0800

    modified:   plugins/fzf-tab.zsh

--- plugins/fzf-tab.zsh
+++ plugins/fzf-tab.zsh
@@ -1,7 +1,13 @@
-# 模糊查找
+# fzf配置
 FZF_PATH=$(dirname "$(which fzf)")
 if [[ -d "$FZF_PATH" ]]; then
     [[ ! :"$PATH": == *:"$FZF_PATH":* ]] && export PATH="${PATH:+${PATH}:}$FZF_PATH"
     source <(fzf --zsh)
 fi
+# 更改映射
+bindkey -r '^T'
+bindkey '\ef' fzf-file-widget
+bindkey -r '^R'
+bindkey '\er' fzf-history-widget
+# 启用插件
 zinit light Aloxaf/fzf-tab
