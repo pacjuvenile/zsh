1ceb8fb687ed374804e9425f7243470b1848b11d
packjuvenile <3283890195@qq.com>
2026-03-02 15:50:43 +0800

    modified:   zsh/custom/plugins/fzf-tab.zsh

--- zsh/custom/plugins/fzf-tab.zsh
+++ zsh/custom/plugins/fzf-tab.zsh
@@ -5,9 +5,8 @@ if [[ -d "$FZF_PATH" ]]; then
     source <(fzf --zsh)
 fi
 # 更改映射
-bindkey -r '^T'
-bindkey '\et' fzf-file-widget
-bindkey -r '^R'
-bindkey '\er' fzf-history-widget
+bindkey '^t' fzf-file-widget
+bindkey '^r' fzf-history-widget
+bindkey -r '\ec'
 # 启用插件
 zinit light Aloxaf/fzf-tab
