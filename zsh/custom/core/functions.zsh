98f0c797051938714970e3579485e7cf18ca01ca
packjuvenile <3283890195@qq.com>
2026-04-20 10:42:15 +0800

    deleted:    bash/ai/claw-run.bash 	modified:   zsh/custom/core/functions.zsh

--- zsh/custom/core/functions.zsh
+++ zsh/custom/core/functions.zsh
@@ -15,11 +15,11 @@ function e() {
 
 # yazi配置
 function ya() {
-  local tmp="$(mktemp -t "yazi-cwd.XXXXX")" cwd
-  command yazi "$@" --cwd-file="$tmp"
-  IFS= read -r -d '' cwd < "$tmp"
-  [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
-  rm -f -- "$tmp"
+	local tmp="$(mktemp -t "yazi-cwd.XXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
 }
 
 # 使用opencode
@@ -50,19 +50,19 @@ function claw-chat() {
 
 # powershell调用
 function pwsh() {
-  powershell.exe -Command "Start-Process powershell.exe -Verb RunAs -ArgumentList '-Command', '$1' -WindowStyle Hidden"
+	powershell.exe -Command "Start-Process powershell.exe -Verb RunAs -ArgumentList '-Command', '$1' -WindowStyle Hidden"
 }
 
 # 痕迹清理
 function clean-tracks() {
-    rm -rf /mnt/c/Users/sunny/AppData/Roaming/Microsoft/Windows/Recent/*
-    rm -rf /mnt/c/Users/sunny/AppData/Roaming/Microsoft/Office/Recent/*
-    rm -rf /mnt/c/Users/sunny/AppData/Roaming/kingsoft/office6/backup/*
-    rm -rf /mnt/c/Users/sunny/AppData/Roaming/Adobe/Common/"Media Cache"/*
-    rm -rf /mnt/c/Users/sunny/AppData/Roaming/Adobe/Common/"Media Cache Files"/*
-    rm -rf /mnt/c/Users/sunny/AppData/Roaming/Adobe/Common/"Peak Files"/*
-    rm -rf /mnt/c/Users/sunny/AppData/Local/gif123/*.gif
-    rm -rf /mnt/c/Users/sunny/Documents/"Tencent Files"/3648579049
-    rm -rf /mnt/c/Users/sunny/Documents/"WeChat Files"/wxid_fp83u8nabg7i22/FileStorage/*
-    rm -rf /mnt/c/Users/sunny/Pictures/QQplayerPic/*
+	rm -rf /mnt/c/Users/sunny/AppData/Roaming/Microsoft/Windows/Recent/*
+	rm -rf /mnt/c/Users/sunny/AppData/Roaming/Microsoft/Office/Recent/*
+	rm -rf /mnt/c/Users/sunny/AppData/Roaming/kingsoft/office6/backup/*
+	rm -rf /mnt/c/Users/sunny/AppData/Roaming/Adobe/Common/"Media Cache"/*
+	rm -rf /mnt/c/Users/sunny/AppData/Roaming/Adobe/Common/"Media Cache Files"/*
+	rm -rf /mnt/c/Users/sunny/AppData/Roaming/Adobe/Common/"Peak Files"/*
+	rm -rf /mnt/c/Users/sunny/AppData/Local/gif123/*.gif
+	rm -rf /mnt/c/Users/sunny/Documents/"Tencent Files"/3648579049
+	rm -rf /mnt/c/Users/sunny/Documents/"WeChat Files"/wxid_fp83u8nabg7i22/FileStorage/*
+	rm -rf /mnt/c/Users/sunny/Pictures/QQplayerPic/*
 }
